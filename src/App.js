da2a0ed1dd51dfcd171cb2113ae78569ceb0a508
Suman_Das <95850019+sumandas0404@users.noreply.github.com>
2023-05-28 17:11:50 +0530

    Add files via upload
    Play-with Love_react

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+import  data from './data'
+
+import Tours from "./Components/Tours";
+const App = () => {
+
+  const [tours,setTours]= useState(data);
+  function removeTour(id){
+      const newTours= tours.filter(tour => tour.id !==id);
+      setTours(newTours);
+  }    
+  if(tours.length===0){
+    return (
+      <div className="refresh">
+        <h2>No Tours Left</h2>
+        <button className="btn-white" onClick={()=> setTours(data)}>
+          Refresh
+        </button>
+      </div>
+    )    
+  }
+  return (
+    <div className="App">
+      {/* <h2>Plan with Love</h2> */}
+      <Tours tours={tours} removeTour={removeTour}></Tours>
+    </div>
+  )
+};
+
+export default App;
